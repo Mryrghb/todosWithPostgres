41ee9f91034dca7c0c46e3505cd0106014a5300a
mrymahmadi <mrymrghb82@gmail.com>
2024-07-26 13:21:14 +0330

    fixed signIn op

--- src/controllers/auths.ts
+++ src/controllers/auths.ts
@@ -1,7 +1,6 @@
 // Import necessary types from Express
-import { Request, response, Response } from "express";
+import { Request, Response } from "express";
 import bcrypt from "bcrypt";
-import { QueryResult } from "pg";
 import jwt from "jsonwebtoken";
 
 // Import the PostgreSQL connection pool from database.ts
@@ -10,7 +9,6 @@ import { pool } from "../database";
 // Controller for a new task
 export const signUp = async (req: Request, res: Response) => {
   // Extract user details from the request body
-  //(fullName, email,password)
   const { fullname, password, userlevel } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
   // Execute a SQL INSERT statement
@@ -31,23 +29,31 @@ export const signUp = async (req: Request, res: Response) => {
 };
 
 export const signIn = async (req: Request, res: Response) => {
-  console.log("1");
   const { userlevel, password } = req.body;
   try {
-    console.log("2");
-    const resu = await pool.query(
-      `SELECT * FROM public."userss" WHERE userlevel = $1`,
+    const result = await pool.query(
+      `SELECT * FROM public."users" WHERE userlevel = $1`,
       [userlevel]
     );
-    const user = resu.rows[0];
-    console.log(user);
-    if (user && (await bcrypt.compare(password, user.password))) {
+
+    const user = result.rows[0];
+
+    //this have ERROR in process.env.JWTSECRET
+    /* if (user && (await bcrypt.compare(password, user.password))) {
       const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET!, {
         expiresIn: "1h",
       });
-      res.json({ token });
+      res.json({ token });*/
+
+    if (await bcrypt.compare(password, user.password)) {
+      const accessToken = jwt.sign({ userId: user.id }, "secret", {
+        expiresIn: "1h",
+      });
+      res
+        .status(200)
+        .json({ welcomeBack: user.fullName, yourToken: accessToken });
     } else {
-      res.status(401).json({ error: "not token" });
+      res.status(401).json({ error: "token error" });
     }
   } catch (error) {
     res.status(500).json({ message: "sign in error" });
